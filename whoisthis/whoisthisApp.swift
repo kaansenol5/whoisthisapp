ed358d4e6158d6f6b0d28884d06ee3b01a87d66b
kaan <kaansnl@icloud.com>
2024-03-02 19:43:37 +0300

    Initial Commit

---
+++ whoisthis/whoisthisApp.swift
@@ -0,0 +1,17 @@
+//
+//  whoisthisApp.swift
+//  whoisthis
+//
+//  Created by Kaan Şenol on 2.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct whoisthisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
